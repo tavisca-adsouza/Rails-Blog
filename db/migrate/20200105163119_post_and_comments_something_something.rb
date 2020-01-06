3c0dd50388fdf54bd6173cae853bc258ca5adcc8
tavisca-adsouza <adsouza@tavisca.in>
2020-01-06 09:38:22 +0530

    fixed the edit and delete button

---
+++ db/migrate/20200105163119_post_and_comments_something_something.rb
@@ -0,0 +1,4 @@
+class PostAndCommentsSomethingSomething < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_104644) do
+ActiveRecord::Schema.define(version: 2020_01_05_163119) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
